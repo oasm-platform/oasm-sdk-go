32153da529f2913bd23852517737170982ac5e5a
anhnmt <anhng.dev@gmail.com>
2025-08-28 13:48:36 +0700

    Add client, client_options

--- oasm/client_options.go
+++ oasm/client_options.go
@@ -1,18 +1,31 @@
 package oasm
 
+import (
+	"net/url"
+	"strings"
+)
+
 // Option represents a functional option for configuring the Client.
-type Option func(*Client)
+type Option func(*Client) error
 
 // WithApiURL sets the base API URL for the Client.
-func WithApiURL(apiUrl string) func(*Client) {
-	return func(c *Client) {
-		c.apiURL = apiUrl
+func WithApiURL(apiUrl string) Option {
+	return func(c *Client) error {
+		parsed, err := url.Parse(apiUrl)
+		if err != nil {
+			return err
+		}
+
+		parsed.Path = strings.TrimRight(parsed.Path, "/")
+		c.apiURL = parsed.String()
+		return nil
 	}
 }
 
 // WithApiKey sets the API key for the Client.
-func WithApiKey(apiKey string) func(*Client) {
-	return func(c *Client) {
+func WithApiKey(apiKey string) Option {
+	return func(c *Client) error {
 		c.apiKey = apiKey
+		return nil
 	}
 }
