d0ea9e7970216df956ac9a32aac75a176db2c6d2
mizhm <minhnt141204@gmail.com>
2025-10-06 10:27:55 +0700

    feat(job-registry): add JobsResult

--- job_registry.go
+++ job_registry.go
@@ -52,3 +52,40 @@ func (c *Client) JobsNext(param *JobsNextParam, header *JobsNextHeader) (*JobsNe
 
 	return &data, nil
 }
+
+type JobsResultParam struct {
+	WorkerID string
+}
+
+type JobsResultRequest struct {
+	JobID string `json:"jobId,omitempty"`
+	Data  struct {
+		Error   bool   `json:"error,omitempty"`
+		Raw     string `json:"raw,omitempty"`
+		Payload any    `json:"payload,omitempty"`
+	} `json:"data,omitempty"`
+}
+
+func (c *Client) JobsResult(param *JobsResultParam, req *JobsResultRequest) error {
+	reqBody, err := sonic.Marshal(req)
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.req.Post(c.apiURL+"/api/jobs-registry/"+param.WorkerID+"/result", "application/json", reqBody)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return ErrorResponse(body)
+	}
+
+	return nil
+}
